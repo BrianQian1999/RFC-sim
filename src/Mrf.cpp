a260273c8cfe02c2b1c04a06a5e1d5a5c12fdd13
qiranq <qiranq@ug.kth.se>
2023-08-28 20:52:05 +0800

    minor updata

---
+++ src/Mrf.cpp
@@ -0,0 +1,21 @@
+#include "Mrf.h"
+
+void Mrf::clear() {
+    stat->clear();
+    iStat->clear();
+}
+
+void Mrf::req(bool rd) noexcept {
+    if (rd) stat->mrfRdNum += mask.count();
+    else stat->mrfWrNum += mask.count();
+}
+
+void Mrf::exec(const TraceInst & inst) {
+    this->mask = inst.mask;
+    iStat->totInstNum++;
+    iStat->mmaInstNum += (uint64_t) (inst.opcode==op::OP_HMMA || inst.opcode==op::OP_IMMA || inst.opcode==op::OP_BMMA);
+   
+    for(const auto & reg : inst.regPool) {
+        req(reg.regType == reg::RegOprdT::SRC);
+    }
+}
