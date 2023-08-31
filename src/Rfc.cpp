71ec4aefb59d2a1a05d115f86b48e9f20f399272
qiranq <qiranq@ug.kth.se>
2023-08-31 22:54:46 +0800

    fix typo

--- src/Rfc.cpp
+++ src/Rfc.cpp
@@ -180,7 +180,7 @@ void Rfc::fullCplAlloc(const reg::RegOprd & oprd) {
     } 
     else if (oprd.regType == reg::RegOprdT::SRC) {
         if (flags.test(oprd.regPos)) {
-            auto p = replWrapper(oprd.regPos);
+            auto p = replWrapper(oprd.regPos % (8 / cam->assoc));
             cam->mem.at(p.second).set(oprd.regIndex, 1, 1, false);
         
             scb->rfcWrNum += mask.count();
